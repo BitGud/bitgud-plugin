4523fb74561860129ec5f8e111cbe0cf8dbbcc52
Bob <48901347+BobTheSoftwareDeveloper@users.noreply.github.com>
2021-07-31 16:05:14 +1200

    Allow plugin to send data to API

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import scms from './scms'
+import axios from 'axios'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const main = (currentDirectory, { since, staged, branch, onFoundSinceRevision, onFoundChangedFiles } = {}) => {
   const scm = scms(currentDirectory)
@@ -8,16 +12,35 @@ const main = (currentDirectory, { since, staged, branch, onFoundSinceRevision, o
   const directory = scm.rootDirectory
 
   const revision = since || scm.getSinceRevision(directory, { staged, branch })
+  const longRevision = scm.getLongHash(directory, { staged, branch })
 
   onFoundSinceRevision && onFoundSinceRevision(scm.name, revision)
 
   const changedFiles = scm.getChangedFiles(directory, revision, staged)
+  const linesChanged = scm.getLinesChanged(directory, revision, staged) || 0
 
   onFoundChangedFiles && onFoundChangedFiles(changedFiles)
 
   const failReasons = new Set()
 
-  scm.getGitEmail(directory)
+  const gitEmail = scm.getGitEmail(directory)
+  const gitName = scm.getGitName(directory)
+  const remoteOrigin = scm.getRemoteOrigin(directory)
+  const currentBranchName = scm.getCurrentBranchName(directory)
+
+  const payload = {
+    userName: gitName,
+    userEmail: gitEmail,
+    commitHash: longRevision,
+    filesChanged: changedFiles.length,
+    linesChanged: linesChanged,
+    branchName: currentBranchName,
+    commitMessage: '',
+    remoteOrigin: remoteOrigin,
+  }
+
+  // Send data to API
+  axios.post(`${process.env.API_URL}/commit/add`, payload)
 
   return {
     success: failReasons.size === 0,
--- src/scms/git.js
+++ src/scms/git.js
@@ -24,12 +24,26 @@ const runGit = (directory, args) =>
     cwd: directory,
   })
 
+const getOutputFromGit = (obj) => String(obj.stdout).trim()
+
 const getLines = (execaResult) => execaResult.stdout.split('\n')
 
 export const getSinceRevision = (directory, { staged, branch }) => {
   try {
     const revision = staged ? 'HEAD' : runGit(directory, ['merge-base', 'HEAD', branch || 'master']).stdout.trim()
     return runGit(directory, ['rev-parse', '--short', revision]).stdout.trim()
+  } catch (error) {
+    if (/HEAD/.test(error.message) || (staged && /Needed a single revision/.test(error.message))) {
+      return null
+    }
+    throw error
+  }
+}
+
+export const getLongHash = (directory, { staged, branch }) => {
+  try {
+    const revision = staged ? 'HEAD' : runGit(directory, ['merge-base', 'HEAD', branch || 'master']).stdout.trim()
+    return runGit(directory, ['rev-parse', revision]).stdout.trim()
   } catch (error) {
     if (/HEAD/.test(error.message) || (staged && /Needed a single revision/.test(error.message))) {
       return null
@@ -50,12 +64,41 @@ export const getChangedFiles = (directory, revision, staged) => {
   ].filter(Boolean)
 }
 
+export const getLinesChanged = (directory, revision, staged) => {
+  const output = getLines(runGit(directory, ['diff', '--numstat']))
+
+  let totalLinesChanged = 0
+
+  for (const obj of output) {
+    let [added, removed, fileName] = obj.split('\t')
+    added = parseInt(added, 10)
+    removed = parseInt(removed, 10)
+
+    if (!isNaN(added) && !isNaN(removed)) {
+      totalLinesChanged += added + removed
+    }
+  }
+  return totalLinesChanged
+}
+
 export const getUnstagedChangedFiles = (directory) => {
   return getChangedFiles(directory, null, false)
 }
 
 export const getGitEmail = (directory) => {
-  return runGit(directory, ['config', 'user.email'])
+  return getOutputFromGit(runGit(directory, ['config', 'user.email']))
+}
+
+export const getGitName = (directory) => {
+  return getOutputFromGit(runGit(directory, ['config', 'user.name']))
+}
+
+export const getRemoteOrigin = (directory) => {
+  return getOutputFromGit(runGit(directory, ['config', '--get', 'remote.origin.url']))
+}
+
+export const getCurrentBranchName = (directory) => {
+  return getOutputFromGit(runGit(directory, ['rev-parse', '--abbrev-ref', 'HEAD']))
 }
 
 export const stageFile = (directory, file) => {
